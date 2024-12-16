9f346fb0d144165a36adb81ccd7adb3b7b80c346
Pham Thanh Tung <tungpt244@gmail.com>
2024-12-16 14:14:39 +0700

    .

--- cron.js
+++ cron.js
@@ -1,90 +1,72 @@
+const axios = require("axios");
+require("dotenv").config();
+
 const sendMessage = async (text) => {
-  const bot_token = "7566091250:AAGeldt1IavD19R7VU9cZBPXevnoBq4kfhY";
-  const chat_id = "-1002462650527";
+  const bot_token = process.env.TELEGRAM_BOT_TOKEN;
+  const chat_id = process.env.TELEGRAM_CHAT_ID;
 
   const baseUrl = `https://api.telegram.org/bot${bot_token}/sendMessage`;
-  await fetch(baseUrl, {
-    method: "POST",
-    body: JSON.stringify({
-      chat_id,
-      text,
-    }),
-    headers: {
-      "Content-Type": "application/json",
-    },
+  await axios.post(baseUrl, {
+    chat_id,
+    text,
   });
 };
 
 const getIPAddress = async () => {
-  let current_ip = "";
-  await fetch("https://api.ipify.org?format=json", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((v) => v.json())
-    .then((data) => {
-      current_ip = data.ip;
-    });
-  return current_ip;
+  const { data } = await axios.get("https://api.ipify.org?format=json");
+  return data.ip;
 };
 
 const getCurrentSUI = async (address) => {
-  let res = "";
-  await fetch("https://wallet-rpc.testnet.sui.io/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "client-sdk-type": "typescript",
-      "client-target-api-version": "1.40.0",
-      "client-request-method": "suix_getAllBalances",
-    },
-    body: JSON.stringify({
+  const headers = {
+    "Content-Type": "application/json",
+    "client-sdk-type": "typescript",
+    "client-target-api-version": "1.40.0",
+    "client-request-method": "suix_getAllBalances",
+  };
+  const { data } = await axios.post(
+    "https://wallet-rpc.testnet.sui.io/",
+    {
       jsonrpc: "2.0",
       id: "2",
       method: "suix_getAllBalances",
       params: [address],
-    }),
-  })
-    .then((response) => response.json())
-    .then((result) => {
-      const _res = result.result.find((a) => a.coinType.match("0x2::"));
-      res = Number(_res.totalBalance / 1000000000).toFixed(2);
-    });
+    },
+    {
+      headers,
+    }
+  );
+  const _res = data.result.find((a) => a.coinType.match("0x2::"));
+  const res = Number(_res.totalBalance / 1000000000).toFixed(2);
+
   return res;
 };
 
 const address_list = [
   "0xf381b14c813dc5896395e9a34bba77fe6d9262d921be09d0fe9bf05425c4faba",
-  // "0xc0d2e3902dfea07cad49590316f049fc08601d3d756c935aa3ab5e370c46d80b",
 ];
 
-let count = [];
+const faucet = async (address, count) => {
+  const ip = await getIPAddress();
+  const totalSUI = await getCurrentSUI(address);
 
-const faucet = async (address) => {
   try {
-    const ip = await getIPAddress();
-
-    const totalSUI = await getCurrentSUI(address);
-
-    const response = await fetch("https://faucet.testnet.sui.io/v1/gas", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
+    await axios.post(
+      "https://faucet.testnet.sui.io/v1/gas",
+      JSON.stringify({
         FixedAmountRequest: {
           recipient: address,
         },
-      }),
-    });
+      })
+    );
 
-    if (response.status !== 429) {
-      const success_message = `✅ [${address}] - from ${ip} - ${new Date().toLocaleString()} - SUI: ${totalSUI}`;
-      await sendMessage(success_message);
-      console.log(success_message);
-      count[address] = 0;
+    const success_message = `✅ [${address}] - from ${ip} - ${new Date().toLocaleString()} - SUI: ${totalSUI}`;
+    await sendMessage(success_message);
+    console.log(success_message);
+    return;
+  } catch (error) {
+    if (count <= 0) {
+      await sendMessage(`🔥 ${address}] - from ${ip} - failed.`);
       return;
     }
 
@@ -92,19 +74,13 @@ const faucet = async (address) => {
     await sendMessage(error_message);
     console.error(error_message);
 
-    count[address] = (count[address] || 0) + 1;
-    if (count[address] < 3) {
-      setTimeout(() => {
-        faucet(address);
-      }, 1000 * 60);
-    }
-  } catch (error) {
-    console.error("Error occurred while processing faucet:", error);
+    await new Promise((resolve) => setTimeout(resolve, 1000 * 60));
+    return faucet(address, count - 1);
   }
 };
 
 const execute = async () => {
-  await Promise.all(address_list.map((a) => faucet(a)));
+  await Promise.all(address_list.map((a) => faucet(a, 3)));
 };
 
 execute();
