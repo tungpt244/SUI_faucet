87ffed0233df44c3f926f8ccfd529edcaa6d65fa
tungpt244 <tungpt244@gmail.com>
2024-12-17 22:07:58 +0700

    .

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const fn = require("./cron");
+const fn = require("../cron");
 
 app.get("/", (req, res) => {
   fn();
@@ -12,3 +12,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+module.exports = app;
